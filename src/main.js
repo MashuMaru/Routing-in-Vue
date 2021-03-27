4f8058fce8a0dcba229784fd6e36a1d68363d854
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-03-27 08:08:43 +0000

    restructured folders

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
+import NotFound from './pages/NotFound.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
 
 import App from './App.vue';
 const router = createRouter({
